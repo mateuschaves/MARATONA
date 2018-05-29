e67fc1d9c36aa45967919c23e27fe8a286a5dc72
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-05-28 23:46:44 -0300

    questão A - incompleta

---
+++ Maratona de Programação da SBC 2017/A.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+        int n, k, i, o = 1;
+        scanf("%d", &n);
+        i = n - 1;
+
+        // Criando um vetor para armazenar alguma coisa ai.
+        int v[n];
+
+        // Numerando o vetor.
+        for(int x = 0; x <= n; x++ ){
+            v[x] = x + 1;
+        }
+        
+        while(1){
+            // Preciso que isso funcioneeeeeeeeeeeeeeeeeeeeeee
+            
+        }
+
+        system("pause");
+}
