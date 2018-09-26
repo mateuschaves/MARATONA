60596746b7c8aec3288689a3390d81e07e580dbf
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-09-26 15:35:00 -0300

    Questão 1115

---
+++ URI Online Judge/1115.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void main(void){
+    int x, y;
+    while(1){
+        scanf("%d%d", &x, &y);
+        if (x == 0 || y == 0)
+            break;
+        if(x > 0 && y > 0)
+            printf("primeiro\n");
+        else if (x < 0 && y > 0)
+            printf("segundo\n");
+        else if (x < 0 && y < 0)
+            printf("terceiro\n");
+        else
+            printf("quarto\n");
+    }
+}
