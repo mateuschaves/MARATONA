b31441c9c8e459b64f56f8a6f9fafbd3be8d9eb2
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-21 13:33:52 -0300

    1036

--- URI Online Judge/1036.py
+++ URI Online Judge/1036.py
@@ -1,11 +1,11 @@
-import math
+# -*- coding: utf-8 -*-
 
 data = input()
 data = [ float(i) for i in data.split() ]
 a, b, c = data
-delta = pow(b, 2) - (4*a*c)
+delta = (b ** 2) - (4*a*c)
 if a == 0 or delta < 0:
     print('Impossivel calcular')
 else:
-    print('R1 = {}'.format(( (-b) + math.sqrt(delta)) / 2 * a ))
-    print('R2 = {}'.format(( (-b) - math.sqrt(delta)) / 2 * a ))
+    print('R1 = {:.5f}'.format(( ((-b) + delta ** (1/2)) / (2 * a) )))
+    print('R2 = {:.5f}'.format(( ((-b) - delta ** (1/2)) / (2 * a) )))
